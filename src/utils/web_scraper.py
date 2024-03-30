549d1874e1c3ee1579d877502303927fde616dc8
Nikolas Giannakopoulos <nickgiann@outlook.com>
2024-03-30 17:36:07 +0100

    Basic preprocessing

--- src/utils/web_scraper.py
+++ src/utils/web_scraper.py
@@ -8,33 +8,34 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from tqdm import tqdm
 
+
 class WebScraper:
     def __init__(self, base_url):
         self.base_url = base_url
         self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         self.headless = True
         self.driver = None
-        
+
     def init_driver(self):
         options = Options()
         options.add_argument(f"user-agent={self.user_agent}")
-        
+
         if self.headless:
             options.add_argument("headless")
-            
+
         return webdriver.Chrome(options=options)
-    
+
     def fetch_article_links(self, url):
         n_scrolls = 63
         x_path_link = '//*[@id="td-outer-wrap"]//div[2]/h3/a'
         x_path_btn_pattern = '//*[@id="td-outer-wrap"]/div[3]/div/div/div[1]/div/div[{}]/a'
 
         self.driver.get(url)
-        
+
         for _ in range(5):
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
-        
+
         for no in tqdm(range(n_scrolls), desc="Fetching Links"):
             try:
                 button_xpath = x_path_btn_pattern.format(32 + no * 10)
@@ -43,13 +44,13 @@ class WebScraper:
                 time.sleep(1.5)
             except NoSuchElementException:
                 continue
-        
+
         elements = self.driver.find_elements(By.XPATH, x_path_link)
         return [element.get_attribute('href') for element in elements]
-    
+
     def fetch_article_contents(self, urls):
         return [self.parse_article(url) for url in tqdm(urls, desc="Parsing Articles")]
-    
+
     @staticmethod
     def parse_article(url):
         try:
@@ -62,30 +63,30 @@ class WebScraper:
         except Exception as e:
             print(f"Error processing {url}: {e}")
             return None
-    
+
     def run(self, category, data_path="", store=True):
         self.driver = self.init_driver()
-        
+
         data_df = None
         file_path = data_path + f"data_{category}.csv"
-        
+
         try:
             data_df = pd.read_csv(file_path)
             print(f"Cached data found for {category}, loading CSV...")
         except FileNotFoundError:
             category_url = self.base_url + category
-            
+
             links = self.fetch_article_links(category_url)
             articles_data = self.fetch_article_contents(links)
-            
+
             articles_data = [article for article in articles_data if article is not None]
-            
+
             data_df = pd.DataFrame(articles_data)
-            
+
             if store:
                 print(f"Storing {category} data to {file_path}...")
                 data_df.to_csv(file_path, index=False)
         finally:
             self.driver.quit()
-            
+
         return data_df
