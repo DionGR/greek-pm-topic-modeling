258e70876f3ae6102190ef7357d4e195622323c1
dion <dion.rigatos@gmail.com>
2024-04-07 14:36:07 +0200

    Add basic EDA

---
+++ src/models/bertopic/config/embeddings.py
@@ -0,0 +1,3 @@
+st_models = {"gr_media":  'dimitriz/st-greek-media-bert-base-uncased',
+             "multilingual": 'paraphrase-multilingual-MiniLM-L12-v2',
+             "gr_stsb": 'lighteternal/stsb-xlm-r-greek-transfer'}
