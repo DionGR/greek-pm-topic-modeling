79942a5e57776736c35ccad9b0c6bbb3b681772e
dion <dion.rigatos@gmail.com>
2024-04-10 17:06:41 +0200

    Add BERTopic final noteboo

--- src/models/bertopic/config/model.py
+++ src/models/bertopic/config/model.py
@@ -5,3 +5,13 @@ STOPWORDS = list(set(el_stop).union(set(en_stop)))
 
 TOP_K = 5
 NUM_TOPICS = 30
+N_GRAMS = (1, 2) 
+
+""" BERTopic Configurations """
+ 
+ 
+""" BERTopic Sub-Model Configruations"""
+
+vectorizer_params_dc = {}
+vectorizer_params_st = {}
+
